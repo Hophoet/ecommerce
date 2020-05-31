e2fd4944cc86194325f875503f43f158f7da154b
hophoet <agbakukofi@gmail.com>
2020-05-31 22:22:48 +0000

    remove item from cart

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path, include
+from . import views
+
+app_name='core'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
+
+
+]
